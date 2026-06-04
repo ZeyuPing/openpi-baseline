6cebd247f0063934958c419e1456ed3c76b66878
ZeyuPing <zeyuping928@gmail.com>
2026-06-04 11:05:16 +0800

    Set LD_LIBRARY_PATH in setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -3,6 +3,8 @@ export OPENPI_DATA_HOME="/root/autodl-tmp/challenge/openpi_data"
 export HF_LEROBOT_HOME="/root/autodl-tmp/challenge/hf_lerobot"
 export HF_HOME="/root/autodl-tmp/challenge/hf_home"
 
+export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
+
 echo "Environment variables set:"
 echo "OPENPI_DATA_HOME: $OPENPI_DATA_HOME"
 echo "HF_LEROBOT_HOME: $HF_LEROBOT_HOME"
